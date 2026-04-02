d7277f157c76324cbdd9a02cf4876b79260ad726
suhaib <suhaib.build@gmail.com>
2026-04-02 23:39:33 +0500

    made some changes in JS and Html

--- java.js
+++ java.js
@@ -1,5 +1,19 @@
-const display = document.getElementById("display")
+const displays = document.getElementById("display")
+const buttons = Array.from(document.querySelectorAll("button"));
 
-function appendtoDisplay(input) {
-    display.value += input;
-};
+buttons.forEach(button => (
+    button.addEventListener("click", (e) =>{
+
+        if(e.target.innerText === "C") {
+            displays.value = "";
+        } else if (e.target.innerText === "=") {
+            try {
+            displays.value = eval(displays.value)
+            } catch {
+                displays.value ="Error";
+            }
+        } else if (e.target.innerText) {
+            displays.value += e.target.innerText;
+        }
+    })
+))
